142606ca17a9056b19b25cd49470a97de51d58aa
DarthMooMancer <107283522+DarthMooMancer@users.noreply.github.com>
2025-03-19 20:22:48 -0400

    Adding Configs

--- nvim/lua/config/opts.lua
+++
@@ -1,7 +0,0 @@
-vim.opt.completeopt = { "menuone", "noselect", "noinsert" }
-vim.opt.mouse =  ""
-vim.opt.wrap = false
-vim.opt.relativenumber = true
-vim.opt.scrolloff = 8
-vim.opt.shiftwidth = 4
-
