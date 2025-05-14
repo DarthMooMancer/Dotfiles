4b234515d8d266af79f418cabc88510f7a4ebf7b
DarthMooMancer <107283522+DarthMooMancer@users.noreply.github.com>
2025-05-14 10:40:10 -0400

    removed: lazydev

--- .config/nvim/lua/darthmoomancer/plugins.lua
+++ .config/nvim/lua/darthmoomancer/plugins.lua
@@ -100,14 +100,5 @@ return {
 		}
 	    }
 	}
-    },
-	--    {
-	-- "folke/lazydev.nvim",
-	-- ft = "lua", -- only load on lua files
-	-- opts = {
-	--     library = {
-	-- 	{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
-	--     },
-	-- },
-	--    },
+    }
 }
